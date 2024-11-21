611a469f4ba856661197e6e0abd1c79c32e5e1cb
Matheus Rosendo <matheus.rosendo@my-oli.com>
2024-11-21 14:52:39 -0300

    feat: included network minikube

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -81,6 +81,14 @@ const config = {
       accounts: [deployerPrivateKey, nodePrivateKey, cpoPrivateKey, emspPrivateKey],
       loggingEnabled: true,
     },
+    minikube: {
+      url: `hardhat:8555`,
+      chainId: 31337,
+      live: false,
+      saveDeployments: true,
+      accounts: [deployerPrivateKey, nodePrivateKey, cpoPrivateKey, emspPrivateKey],
+      loggingEnabled: true,
+    },
   },
   abiExporter: {
     path: "./abi",
